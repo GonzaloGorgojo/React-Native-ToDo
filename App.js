9187cf19b351687423c42b5211ca17ee714f67dd
Gonzalo Gorgojo <gongorgojo@gmail.com>
2021-06-09 00:14:01 -0300

    Add Readme

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
+  const [enteredTodo, setEnteredTodo] = useState("");
+  const [showTodo, setShowTodo] = useState([]);
+
+  const todoInputHandler = (inputText) => {
+    setEnteredTodo(inputText);
+  };
+
+  const addTodoHandler = () => {
+    setShowTodo((showTodo) => [...showTodo, enteredTodo]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainScreen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="ToDO List"
+          style={styles.input}
+          onChangeText={todoInputHandler}
+          value={enteredTodo}
+        />
+        <Button title="ADD" color="green" onPress={addTodoHandler} />
+      </View>
+      <ScrollView>
+        {showTodo.map((goal, i) => (
+          <View key={i} style={styles.list}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    width: 200,
+    margin: 5,
+    paddingLeft: 10,
+    borderColor: "black",
+    borderWidth: 2,
+  },
+  mainScreen: {
+    paddingTop: 30,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  list: {
+    marginVertical: 10,
+    padding: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
   },
 });
---
+++ readme.md
@@ -0,0 +1,27 @@
+# ToDo List :clipboard:
+
+### 👉 What is this ?
+
+This is an APP where you can make your own todo list, grocery list, etc.
+
+### :computer: How this was made ?
+
+- React-Native
+- Expo
+
+### :calling: prerequisites
+
+- You need to have installed Expo CLI globally.<br>
+  Options:
+- You can use Android Studio.
+- Android device with Expo installed.
+- IOS device with Expo installed.
+
+### :bulb: How do i use this ?
+
+1. Download/Fork/Clone.
+2. NPM Install
+3. NPM start
+4. Choose your device and start using it !
+
+#### This was made by [Gonzalo Gorgojo !](https://www.linkedin.com/in/gonzalogorgojo/) feel free to contact me for everything you need !
